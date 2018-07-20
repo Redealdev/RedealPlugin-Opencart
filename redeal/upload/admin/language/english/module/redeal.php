3ab4f946965c177cda14107d0b34abdfd6221d56
Bhavik Mehta <bhavik.mehta@techcronus.com>
2018-07-20 14:22:38 +0530

    Opencart plugin deployment.
    Opencart plugin deployment.

---
+++ redeal/upload/admin/language/english/module/redeal.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']       = 'Redeal';
+ 
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module Redeal!';
+ 
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module Redeal!';
+$_['error_code']          = 'Code Required';
+?>
---
+++ redeal/upload/admin/model/sale/redealorderdetails.php
@@ -0,0 +1,10 @@
+<?php
+
+class ModelRedealOrder extends Model {
+    public function getOrderTotals($order_id) {
+            $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "order_total WHERE order_id = '" . (int)$order_id . "' ORDER BY sort_order");
+            return $query->rows;
+    }
+}
+
+?>
